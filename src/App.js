e9d82508bf51ae4538748b68d274c577d1e8da5f
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-06 14:05:20 -0400

    Basis added. Now going to start coding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react'
 
 class TodoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [],
+      currentToDo:"",
+    };
+  }
+
+  //This is where the methods go
+  handleChange = event => {
+    console.log(event.target.value)
+
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    console.log("addItem Method fired")
+  };
   render(){
     return (
       <div className='container'>
+        <form onSummit={this.addItem}>
+          <label htmlFor="tastName">Task Name:</label>
+          <input onChange={this.handleChange} name="taskName"type="text"placeholder="Add todo here!"/>
+          <button type="submit">Add Task</button>
+        </form>
 
       </div>
-    )
+    );
   }
 }
 export default TodoList;
