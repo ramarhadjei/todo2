c694f0329629a23b72978be5be48af39d0f2f345
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-06 16:12:19 -0400

    Finished edge question 1 Completethe handleChange and the addItem methods

--- src/App.js
+++ src/App.js
@@ -9,17 +9,35 @@ class TodoList extends Component {
     };
   }
 
-  //This is where the methods go
+  //This is where I'm going to code the handleChange method the methods go
   handleChange = event => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
+    this.setState({
+      currentToDo: event.target.value,
 
+    })
+    //console.log(this.state.currentToDo);
   };
-
+//this is where I am going to code the addItem method
   addItem = event => {
     event.preventDefault();
-    console.log("addItem Method fired")
+   // console.log("addItem Method fired")
+   if(this.state.currentToDo !==''){
+     this.setState({
+       currentToDo: "",
+       //ES6 Workshop-spread operator. Can also push it in
+       todos: [...this.state.todos, this.state.currentToDo]
+
+     })
+
+   }
   };
+
+
   render(){
+   // console.log(this.state.todos)
+   //deconstruct todos from state
+   const {todos} = this.state
     return (
       <div className='container'>
         <form onSummit={this.addItem}>
@@ -27,6 +45,15 @@ class TodoList extends Component {
           <input onChange={this.handleChange} name="taskName"type="text"placeholder="Add todo here!"/>
           <button type="submit">Add Task</button>
         </form>
+<ul>
+  {todos.map((el,idx) => {
+    return(
+      <li key={idx}>
+        {el}
+      </li>
+    )
+  })}
+</ul>
 
       </div>
     );
