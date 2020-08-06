40461d4162113ff67d8ad62053947b7e12dcda74
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-06 18:45:08 -0400

    Completed Question 3 on delete

--- src/App.js
+++ src/App.js
@@ -33,6 +33,16 @@ class TodoList extends Component {
    }
   };
 
+  deleteItem = (idx) => {
+    //console.log(idx)
+    const filteredItems = this.state.todos.filter((el, index) => {
+      return index !== idx
+    })
+    //console.log(filteredItems)
+    this.setState({
+      todos: filteredItems,
+    })
+  }
 
   render(){
    // console.log(this.state.todos)
@@ -40,7 +50,7 @@ class TodoList extends Component {
    const {todos} = this.state
     return (
       <div className='container'>
-        <form onSummit={this.addItem}>
+        <form onSumit={this.addItem}>
           <label htmlFor="tastName">Task Name:</label>
           <input onChange={this.handleChange} name="taskName"type="text"placeholder="Add todo here!"/>
           <button type="submit">Add Task</button>
@@ -50,6 +60,7 @@ class TodoList extends Component {
     return(
       <li key={idx}>
         {el}
+        <button type="button" onClick={() => this.deleteItem(idx)} ></button>
       </li>
     )
   })}
